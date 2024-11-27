f542acad4b71166228bd32794d7877c29e0ad2d5
pluuulll <nikovergara2017@gmail.com>
2024-11-27 10:29:20 -0300

    vluu-db

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -12,10 +12,17 @@ export class RegistroPage {
     usuario: '',
     contrasena: ''
   };
+  showErrorMessage: boolean = false; // Controla la visibilidad del mensaje de error
 
   constructor(private supabaseService: SupabaseService, private router: Router) {}
 
   async register() {
+    // Verificar si los campos están vacíos
+    if (this.user.usuario.trim() === '' || this.user.contrasena.trim() === '') {
+      this.showErrorMessage = true; // Mostrar el mensaje de error si algún campo está vacío
+      return;
+    }
+
     try {
       // Intentar registrar al usuario en la base de datos
       await this.supabaseService.registerUser(this.user.usuario, this.user.contrasena);
