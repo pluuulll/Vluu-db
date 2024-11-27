7463aff1af76601bfb5018335df20391e8e19012
pluuulll <nikovergara2017@gmail.com>
2024-11-27 01:03:51 -0300

    vluu-db

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,55 +3,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // Rutas principales
-  { 
-    path: '', 
-    redirectTo: 'login', // Redirección predeterminada
-    pathMatch: 'full' 
-  },
-  { 
-    path: 'inicio', 
-    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule) 
-  },
-  { 
-    path: 'login', 
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) 
-  },
-  { 
-    path: 'registro', 
-    loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule) 
-  },
-  { 
-    path: 'recuperar', 
-    loadChildren: () => import('./pages/recuperar/recuperar.module').then(m => m.RecuperarPageModule) 
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
+  { path: 'registro', loadChildren: () => import('./pages/registro/registro.module').then(m => m.RegistroPageModule) },
+  { path: 'recuperar', loadChildren: () => import('./pages/recuperar/recuperar.module').then(m => m.RecuperarPageModule) },
 
   // Rutas de navegación
-  { 
-    path: 'perfil', 
-    loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule) 
-  },
-  { 
-    path: 'chatbot', 
-    loadChildren: () => import('./pages/chatbot/chatbot.module').then(m => m.ChatbotPageModule) 
-  },
-  { 
-    path: 'escanear', 
-    loadChildren: () => import('./pages/escanear/escanear.module').then(m => m.EscanearPageModule) 
-  },
-  { 
-    path: 'recomendaciones', 
-    loadChildren: () => import('./pages/recomendaciones/recomendaciones.module').then(m => m.RecomendacionesPageModule) 
-  },
-  {
-    path: 'mostrardatos',
-    loadChildren: () => import('./pages/mostrardatos/mostrardatos.module').then( m => m.MostrardatosPageModule)
-  }
+  { path: 'inicio', loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule) },
+  { path: 'perfil', loadChildren: () => import('./pages/perfil/perfil.module').then(m => m.PerfilPageModule) },
+  { path: 'chatbot', loadChildren: () => import('./pages/chatbot/chatbot.module').then(m => m.ChatbotPageModule) },
+  { path: 'escanear', loadChildren: () => import('./pages/escanear/escanear.module').then(m => m.EscanearPageModule) },
+  { path: 'recomendaciones', loadChildren: () => import('./pages/recomendaciones/recomendaciones.module').then(m => m.RecomendacionesPageModule) },
+  { path: 'mostrardatos', loadChildren: () => import('./pages/mostrardatos/mostrardatos.module').then(m => m.MostrardatosPageModule) },
+
+  // Páginas de error
+  { path: 'e404', loadChildren: () => import('./pages/e404/e404.module').then(m => m.E404PageModule) },
+
+  // Ruta comodín para páginas no encontradas (SIEMPRE AL FINAL)
+  { path: '**', redirectTo: 'e404', pathMatch: 'full' }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
