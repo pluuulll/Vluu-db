63a8e4ba6babdcb4d4e00c02c9b4e27ef0133e1a
pluuulll <nikovergara2017@gmail.com>
2024-12-01 20:32:03 -0300

    vluu-db

--- Vluu-db-master/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  supabaseUrl: 'https://fgymapzzxepsrwebwhxj.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZneW1hcHp6eGVwc3J3ZWJ3aHhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1NDEwNDMsImV4cCI6MjA0ODExNzA0M30.efyGRBFB4xEpvFCgyIjJGuPXfZB81c5uQJ2yqsCXXzU',
-};
